e7b1a385356d2a9ace721093e2ebcb2ccb06ab92
Abdulmuiz Adeyemo <abdulmuizproject@gmail.com>
2026-07-11 11:24:36 +0000

    revert: dist should not be tracked

--- dist/sea-entry.cjs
+++
@@ -1,8 +0,0 @@
-const { join } = require("path");
-const { readFileSync } = require("fs");
-let version = "0.3.0";
-try {
-  const pkg = JSON.parse(readFileSync(join(__dirname, "package.json"), "utf-8"));
-  version = pkg.version || "0.3.0";
-} catch {}
-require("./bundle.cjs");
